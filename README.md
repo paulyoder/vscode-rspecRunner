23e86ee6d8681e0b7eec26fa1cc51e82249e92ff
Paul Yoder <paulyoder@gmail.com>
2020-10-22 11:39:45 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# rspecRunner README
+
+Run Rspec tests in the terminal
+
+## Requirements
+
+None
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `rspecRunner.run`: run rspec on the active file
+
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "rspecRunner" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('rspecRunner.run', () => {
+		let document = vscode.window.activeTextEditor?.document;
+		let terminal = vscode.window.terminals[0];
+		if (document == null) {
+			vscode.window.showErrorMessage('No active text editor');
+			return;
+		}
+		if (terminal == null) {
+			vscode.window.showErrorMessage('No active terminal');
+			return;
+		}
+
+		let relativePath = vscode.workspace.asRelativePath(document.uri.path);
+		if (!relativePath.includes('.rb')) {
+			vscode.window.showErrorMessage('Can only run rspec tests on Ruby files');
+			return;
+		}
+		if (!relativePath.includes('_spec.rb')) {
+			relativePath = relativePath.replace('app/', 'spec/').replace('.rb', '_spec.rb');
+		}
+
+		terminal.sendText(`bundle exec rspec ${relativePath}`);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
